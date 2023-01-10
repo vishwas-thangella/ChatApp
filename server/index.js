b42a005133816325d9ec1117a76d40cd51addf65
Vishwas <vishwas56883669@gmail.com>
2023-01-10 23:10:24 +0530

    Changed index

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const ConnectToDB = require('./config/Database');
 const UserRouter = require('./routes/UserRoute');
 const MessageRoute = require('./routes/MessageRoute');
 const cors = require('cors');
-const socket = require('socket.io')
 
 const app = express();
 
@@ -22,27 +21,3 @@ app.use('/api/message',MessageRoute);
 const server = app.listen(process.env.PORT,()=>{
     console.log(colors.red(`server started at port : ${process.env.PORT}`));
 });
-
-const io = socket(server,{
-    cors:{
-        origin:'http://192.168.29.173:3000'
-    },
-    credentials:true,
-});
-
-global.onlineUsers = new Map();
-
-io.on('connection',(socket)=>{
-    global.chatSocket = socket;
-    socket.on('add-user',(userId)=>{
-        onlineUsers.set(userId,socket.id);
-    });
-
-    socket.on('send-msg',(data)=>{
-        const sendUserSocket = onlineUsers.get(data.to);
-        if(sendUserSocket){
-            socket.to(sendUserSocket).emit('msg-recieve',data.message);
-        }
-    });
-});
-
